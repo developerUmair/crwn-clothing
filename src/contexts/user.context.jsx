3d225521508ee848789e47773745cd1eb1c813a5
developerUmair <umairmak143@gmail.com>
2024-10-04 10:38:36 +0500

    add userReducer in user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,9 @@
-import { createContext, useEffect, useState } from "react";
-import { createUserDocumentFromAuth, onAuthStateChangedListener, signOutUser } from "../utils/firebase/firebase.utils";
+import { createContext, useEffect, useReducer } from "react";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+  signOutUser,
+} from "../utils/firebase/firebase.utils";
 
 // as the actual value you wanna access
 export const UserContext = createContext({
@@ -7,21 +11,53 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+export const userReducer = (state, action) => {
+  console.log(action)
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      };
+
+    default:
+      throw new Error(`Unhandled action type: ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+  const { currentUser } = state
+  console.log(currentUser)
+
+  const setCurrentUser = user => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+  }
+
   const value = { currentUser, setCurrentUser };
 
   // signOutUser();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-
-      if(user){
-        createUserDocumentFromAuth(user)
+      if (user) {
+        createUserDocumentFromAuth(user);
       }
       setCurrentUser(user);
-      console.log(user)
-    })
+      console.log(user);
+    });
 
     return () => {
       unsubscribe(); // Unsubscribe when the component unmounts
